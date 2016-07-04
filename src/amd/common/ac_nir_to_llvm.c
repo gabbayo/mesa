ca681478546f8e5f23150939e22a47abfa924067
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2016-07-04 21:02:45 +0200

    amd/common: Setup a simple compute shader ABI.

--- src/amd/common/ac_nir_to_llvm.c
+++ src/amd/common/ac_nir_to_llvm.c
@@ -26,24 +26,67 @@
 
 #include "nir/nir.h"
 
+enum radeon_llvm_calling_convention {
+	RADEON_LLVM_AMDGPU_VS = 87,
+	RADEON_LLVM_AMDGPU_GS = 88,
+	RADEON_LLVM_AMDGPU_PS = 89,
+	RADEON_LLVM_AMDGPU_CS = 90,
+};
+
+#define CONST_ADDR_SPACE 2
+#define LOCAL_ADDR_SPACE 3
+
 struct nir_to_llvm_context {
 	LLVMContextRef context;
 	LLVMModuleRef module;
 	LLVMBuilderRef builder;
+	LLVMValueRef main_function;
+
+	LLVMValueRef descriptor_sets[4];
+	LLVMValueRef workgroup_ids;
+	LLVMValueRef local_invocation_ids;
 
 	LLVMTypeRef i1;
 	LLVMTypeRef i32;
 	LLVMTypeRef f32;
 };
 
-static void ac_llvm_create_func(LLVMContextRef ctx, LLVMModuleRef module,
-                                LLVMBuilderRef builder,
-                                LLVMTypeRef *return_types,
-                                unsigned num_return_elems,
-                                LLVMTypeRef *ParamTypes, unsigned ParamCount)
+static void set_llvm_calling_convention(LLVMValueRef func,
+                                        gl_shader_stage stage)
 {
-	LLVMTypeRef main_fn_type, ret_type;
-	LLVMBasicBlockRef main_fn_body;
+	enum radeon_llvm_calling_convention calling_conv;
+
+	switch (stage) {
+	case MESA_SHADER_VERTEX:
+	case MESA_SHADER_TESS_CTRL:
+	case MESA_SHADER_TESS_EVAL:
+		calling_conv = RADEON_LLVM_AMDGPU_VS;
+		break;
+	case MESA_SHADER_GEOMETRY:
+		calling_conv = RADEON_LLVM_AMDGPU_GS;
+		break;
+	case MESA_SHADER_FRAGMENT:
+		calling_conv = RADEON_LLVM_AMDGPU_PS;
+		break;
+	case MESA_SHADER_COMPUTE:
+		calling_conv = RADEON_LLVM_AMDGPU_CS;
+		break;
+	default:
+		unreachable("Unhandle shader type");
+	}
+
+	LLVMSetFunctionCallConv(func, calling_conv);
+}
+
+static LLVMValueRef
+create_llvm_function(LLVMContextRef ctx, LLVMModuleRef module,
+                     LLVMBuilderRef builder, LLVMTypeRef *return_types,
+                     unsigned num_return_elems, LLVMTypeRef *param_types,
+                     unsigned param_count, unsigned array_params,
+                     unsigned sgpr_params)
+{
+	LLVMTypeRef main_function_type, ret_type;
+	LLVMBasicBlockRef main_function_body;
 
 	if (num_return_elems)
 		ret_type = LLVMStructTypeInContext(ctx, return_types,
@@ -52,10 +95,76 @@ static void ac_llvm_create_func(LLVMContextRef ctx, LLVMModuleRef module,
 		ret_type = LLVMVoidTypeInContext(ctx);
 
 	/* Setup the function */
-	main_fn_type = LLVMFunctionType(ret_type, ParamTypes, ParamCount, 0);
-	LLVMValueRef main_fn = LLVMAddFunction(module, "main", main_fn_type);
-	main_fn_body = LLVMAppendBasicBlockInContext(ctx, main_fn, "main_body");
-	LLVMPositionBuilderAtEnd(builder, main_fn_body);
+	main_function_type =
+	    LLVMFunctionType(ret_type, param_types, param_count, 0);
+	LLVMValueRef main_function =
+	    LLVMAddFunction(module, "main", main_function_type);
+	main_function_body =
+	    LLVMAppendBasicBlockInContext(ctx, main_function, "main_body");
+	LLVMPositionBuilderAtEnd(builder, main_function_body);
+
+	LLVMSetFunctionCallConv(main_function, RADEON_LLVM_AMDGPU_CS);
+	for (unsigned i = 0; i < sgpr_params; ++i) {
+		LLVMValueRef P = LLVMGetParam(main_function, i);
+
+		if (i < array_params)
+			LLVMAddAttribute(P, LLVMByValAttribute);
+		else
+			LLVMAddAttribute(P, LLVMInRegAttribute);
+	}
+	return main_function;
+}
+
+static LLVMTypeRef const_array(LLVMTypeRef elem_type, int num_elements)
+{
+	return LLVMPointerType(LLVMArrayType(elem_type, num_elements),
+	                       CONST_ADDR_SPACE);
+}
+
+static void create_function(struct nir_to_llvm_context *ctx,
+                            struct nir_shader *nir)
+{
+	LLVMTypeRef arg_types[20];
+	unsigned arg_idx = 0;
+	unsigned array_count = 0;
+	unsigned sgpr_count = 0;
+
+	for (unsigned i = 0; i < 4; ++i)
+		arg_types[arg_idx++] = const_array(ctx->i32, 1024 * 1024);
+
+	array_count = arg_idx;
+	switch (nir->stage) {
+	case MESA_SHADER_COMPUTE:
+		arg_types[arg_idx++] = LLVMVectorType(ctx->i32, 3);
+		sgpr_count = arg_idx;
+
+		arg_types[arg_idx++] = LLVMVectorType(ctx->i32, 3);
+		break;
+	default:
+		unreachable("Shader stage not implemented");
+	}
+
+	ctx->main_function = create_llvm_function(
+	    ctx->context, ctx->module, ctx->builder, NULL, 0, arg_types,
+	    arg_idx, array_count, sgpr_count);
+	set_llvm_calling_convention(ctx->main_function, nir->stage);
+
+	arg_idx = 0;
+
+	for (unsigned i = 0; i < 4; ++i)
+		ctx->descriptor_sets[i] =
+		    LLVMGetParam(ctx->main_function, arg_idx++);
+
+	switch (nir->stage) {
+	case MESA_SHADER_COMPUTE:
+		ctx->workgroup_ids =
+		    LLVMGetParam(ctx->main_function, arg_idx++);
+		ctx->local_invocation_ids =
+		    LLVMGetParam(ctx->main_function, arg_idx++);
+		break;
+	default:
+		unreachable("Shader stage not implemented");
+	}
 }
 
 static void setup_types(struct nir_to_llvm_context *ctx)
@@ -74,9 +183,7 @@ LLVMModuleRef ac_translate_nir_to_llvm(LLVMTargetMachineRef tm,
 	setup_types(&ctx);
 
 	ctx.builder = LLVMCreateBuilderInContext(ctx.context);
-	LLVMTypeRef args[] = {ctx.f32, ctx.f32};
-	ac_llvm_create_func(ctx.context, ctx.module, ctx.builder, NULL, 0, args,
-	                    2);
+	create_function(&ctx, nir);
 
 	LLVMBuildRetVoid(ctx.builder);
 	LLVMDisposeBuilder(ctx.builder);
